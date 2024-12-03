c85d4f8d8346f0a9b481788b2223e1099ce5cb6c
szimmerman92 <samuel.e.zimmerman@gmail.com>
2024-12-02 19:42:45 -0500

    fixed bug in confounding analysis

--- R/summarize_linear_output.R
+++ R/summarize_linear_output.R
@@ -46,11 +46,17 @@ find_confounders_linear <- function(voe_list_for_reg){
   if('dependent_feature' %in% colnames(voe_adjust_for_reg_ptype)){ 
     if(!(1 %in% unique(unlist(unname(table(voe_adjust_for_reg_ptype$dependent_feature)))))){
       tryCatch({
-        voe_adjust_for_reg_ptype_split = split(voe_adjust_for_reg_ptype,voe_adjust_for_reg_ptype$term)
-        fit_estimate = purrr::map(voe_adjust_for_reg_ptype_split, ~ lme4::lmer(data=.x %>% dplyr::select_if(~ length(unique(.)) > 1),stats::as.formula(estimate ~ . +(1|dependent_feature) - dependent_feature - estimate - p.value),control = lme4::lmerControl(optimizer = "bobyqa")))
-        fullmod=fit_estimate
-        fit_estimate_forplot = purrr::map(fit_estimate, ~ broom.mixed::tidy(.x) %>% dplyr::mutate(sdmin=(.data$estimate - .data$std.error),sdmax=(.data$estimate + .data$std.error)))
-        },
+        if("term"%in% colnames(voe_adjust_for_reg_ptype)) {
+          voe_adjust_for_reg_ptype_split = split(voe_adjust_for_reg_ptype,voe_adjust_for_reg_ptype$term)
+          fit_estimate = purrr::map(voe_adjust_for_reg_ptype_split, ~ lme4::lmer(data=.x %>% dplyr::select_if(~ length(unique(.)) > 1),stats::as.formula(estimate ~ . +(1|dependent_feature) - dependent_feature - estimate - p.value),control = lme4::lmerControl(optimizer = "bobyqa")))
+          fullmod=fit_estimate
+          fit_estimate_forplot = purrr::map(fit_estimate, ~ broom.mixed::tidy(.x) %>% dplyr::mutate(sdmin=(.data$estimate - .data$std.error),sdmax=(.data$estimate + .data$std.error)))
+        } else {
+          fit_estimate=lme4::lmer(data=voe_adjust_for_reg_ptype,stats::as.formula(estimate ~ . +(1|dependent_feature) - dependent_feature - estimate - p.value),control = lme4::lmerControl(optimizer = "bobyqa"))
+          fullmod=fit_estimate
+          fit_estimate_forplot=broom.mixed::tidy(fit_estimate) %>% dplyr::mutate(sdmin=(.data$estimate-.data$std.error),sdmax=(.data$estimate+.data$std.error))
+        }
+      },
       error = function(e){
         print('Note: Mixed effect modeling to identify sources of confounding failed. Running a simple linear model instead. If you want to try this analysis yourself, you can access the raw data for this yourself in the output and follow the methodological layout in the docs.')
         trylinear <<- TRUE
@@ -58,11 +64,16 @@ find_confounders_linear <- function(voe_list_for_reg){
     }
     if(trylinear==TRUE){
       tryCatch({
-        
-        voe_adjust_for_reg_ptype_split = split(voe_adjust_for_reg_ptype,voe_adjust_for_reg_ptype$term)
-        fit_estimate = purrr::map(voe_adjust_for_reg_ptype_split, ~ stats::lm(data=.x %>% dplyr::select_if(~ length(unique(.)) > 1),stats::as.formula(estimate ~ . - estimate - dependent_feature - p.value)))
-        fullmod=fit_estimate
-        fit_estimate_forplot = purrr::map(fit_estimate, ~ broom::tidy(.x) %>% dplyr::mutate(sdmin=(.data$estimate - .data$std.error),sdmax=(.data$estimate + .data$std.error)))
+        if("term"%in% colnames(voe_adjust_for_reg_ptype)) {
+          voe_adjust_for_reg_ptype_split = split(voe_adjust_for_reg_ptype,voe_adjust_for_reg_ptype$term)
+          fit_estimate = purrr::map(voe_adjust_for_reg_ptype_split, ~ stats::lm(data=.x %>% dplyr::select_if(~ length(unique(.)) > 1),stats::as.formula(estimate ~ . - estimate - dependent_feature - p.value)))
+          fullmod=fit_estimate
+          fit_estimate_forplot = purrr::map(fit_estimate, ~ broom::tidy(.x) %>% dplyr::mutate(sdmin=(.data$estimate - .data$std.error),sdmax=(.data$estimate + .data$std.error)))
+        else {
+          fit_estimate=stats::lm(data=voe_adjust_for_reg_ptype,stats::as.formula(estimate ~ . - estimate - dependent_feature - p.value))
+          fullmod=fit_estimate
+          fit_estimate_forplot=broom::tidy(fit_estimate) %>% dplyr::mutate(sdmin=(.data$estimate - .data$std.error),sdmax=(.data$estimate + .data$std.error))
+        }
       },
       error = function(e){
         fit_estimate_forplot = 'Confounder analysis failed.'
@@ -74,11 +85,16 @@ find_confounders_linear <- function(voe_list_for_reg){
   else{
     tryCatch({
       print('Note: Using regular linear model for confounder analysis instead of a mixed effect one. See the GitHub README for more details.')
-      voe_adjust_for_reg_ptype_split = split(voe_adjust_for_reg_ptype,voe_adjust_for_reg_ptype$term)
-      fit_estimate = purrr::map(voe_adjust_for_reg_ptype_split, ~ stats::lm(data=.x %>% dplyr::select_if(~ length(unique(.)) > 1),stats::as.formula(estimate ~ . - estimate - p.value)))
-      fullmod=fit_estimate
-      fit_estimate_forplot = purrr::map(fit_estimate, ~ broom::tidy(.x) %>% dplyr::mutate(sdmin=(.data$estimate - .data$std.error),sdmax=(.data$estimate + .data$std.error)))
-      
+      if("term"%in% colnames(voe_adjust_for_reg_ptype)) {
+        voe_adjust_for_reg_ptype_split = split(voe_adjust_for_reg_ptype,voe_adjust_for_reg_ptype$term)
+        fit_estimate = purrr::map(voe_adjust_for_reg_ptype_split, ~ stats::lm(data=.x %>% dplyr::select_if(~ length(unique(.)) > 1),stats::as.formula(estimate ~ . - estimate - p.value)))
+        fullmod=fit_estimate
+        fit_estimate_forplot = purrr::map(fit_estimate, ~ broom::tidy(.x) %>% dplyr::mutate(sdmin=(.data$estimate - .data$std.error),sdmax=(.data$estimate + .data$std.error)))
+      else {
+        fit_estimate=stats::lm(data=voe_adjust_for_reg_ptype,stats::as.formula(estimate ~ . - estimate - p.value))
+        fullmod=fit_estimate
+        fit_estimate_forplot=broom::tidy(fit_estimate) %>% dplyr::mutate(sdmin=(.data$estimate - .data$std.error),sdmax=(.data$estimate + .data$std.error))
+      }
     },
     error = function(e){
       fit_estimate_forplot = 'Confounder analysis failed.'
